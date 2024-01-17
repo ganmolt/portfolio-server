b39bfbb1f935f9e5e2b182c41ba19267a5abed81
ganmolt <o.qanm.o@gmail.com>
2024-01-18 07:25:46 +0900

    DB接続

--- app/main.go
+++ app/main.go
@@ -1,18 +1,48 @@
 package main 
 
 import (
-    "github.com/gin-gonic/gin"
+  "os"
+  "fmt"
+  "github.com/gin-gonic/gin"
+  "gorm.io/driver/mysql"
+  "gorm.io/gorm"
 )
 
+type User struct {
+  gorm.Model
+  Id  int `gorm:"primaryKey" json:"id"`
+  Name string `json:"name"`
+  Age  int `json:"age"`
+}
+
 func main() {
-    router := gin.Default()
+  dsn := fmt.Sprintf(
+    "%s:%s@tcp(%s)/%s?tls=true",
+    os.Getenv("DB_USERNAME"),
+    os.Getenv("DB_PASSWORD"),
+    os.Getenv("DB_HOST"),
+    os.Getenv("DB_DATABASE"),
+  )
+  
+  db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
 
-		router.LoadHTMLGlob("templates/*.html")
+  router := gin.Default()
 
-		data := "Hello Go/Gin!!"
-    router.GET("/", func(c *gin.Context) {
-				c.HTML(200, "index.html", gin.H{"data": data})
-    })
+  router.LoadHTMLGlob("templates/*.html")
 
-    router.Run(":3001")
+  data := "Hello Go/Gin!!"
+  router.GET("/", func(c *gin.Context) {
+      c.HTML(200, "index.html", gin.H{"data": data})
+  })
+
+  router.GET("/users", func(c *gin.Context) {
+    var users []User
+    db.Unscoped().Find(&users)
+    c.JSON(200, users)
+  })
+
+  router.Run(":3001")
 }
