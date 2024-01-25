08147d223a75d5cc0a195c6f83461d0089c7398e
ganmolt <o.qanm.o@gmail.com>
2024-01-25 11:45:45 +0900

    allow-originの微修正(/を外した)

--- app/main.go
+++ app/main.go
@@ -27,7 +27,7 @@ func main() {
     // アクセスを許可したいアクセス元
     AllowOrigins: []string{
         "http://localhost:3000",
-        "https://ganmolt.github.io/react-portfolio/",
+        "https://ganmolt.github.io/react-portfolio",
     },
     // アクセスを許可したいHTTPメソッド(以下の例だとPUTやDELETEはアクセスできません)
     AllowMethods: []string{
